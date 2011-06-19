93ba96bd270ed0cd32daacfa20bf75f1d5228e65
Lauris Bukšis-Haberkorns <lafriks@gmail.com>
2011-06-20 00:29:36 +0300

    Bit of reorganization

---
+++ Framework/Web/WebApplication.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * WebApplication class file.
+ *
+ * @author Lauris Bukšis-Haberkorns <lauris@nix.lv>
+ * @link http://framework.nix.lv/
+ * @copyright Copyright &copy; 2011 Nix Software
+ * @license http://framework.nix.lv/license/
+ */
+namespace Nix\Framework\Web;
+
+/**
+ * WebApplication is class for web applications.
+ *
+ * @author Lauris Bukšis-Haberkorns <lauris@nix.lv>
+ * @version $Id$
+ * @since 1.0
+ */
+class WebApplication extends \Nix\Framework\BaseApplication
+{
+	public function Run()
+	{
+		echo 'It works!';
+	}
+}
