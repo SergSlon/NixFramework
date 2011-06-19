189f1035959b5e26f370bd3fa416dc518c74ce17
Lauris Bukšis-Haberkorns <lafriks@gmail.com>
2011-06-20 00:05:45 +0300

    Autoloading classes

--- nix.php
+++ nix.php
@@ -1,6 +1,14 @@
 <?php
+/**
+ * Nix Framework bootstrap file.
+ *
+ * @author Lauris Bukšis-Haberkorns <lauris@nix.lv>
+ * @link http://framework.nix.lv/
+ * @copyright Copyright &copy; 2011 Nix Software
+ * @license http://framework.nix.lv/license/
+ */
+require_once 'Framework/compatibility/SplClassLoader.php';
 
-require_once 'compatibility/SplClassLoader.php';
-
-$nixClassLoader =  new SplClassLoader('Nix', __DIR__);
+// Autoloader for Nix Framework classes
+$nixClassLoader =  new SplClassLoader('Nix', dirname(__DIR__));
 $nixClassLoader->register();
